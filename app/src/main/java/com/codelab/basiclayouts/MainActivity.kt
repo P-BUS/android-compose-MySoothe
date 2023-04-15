67dc40563f715394b7e9388c58ebfffa4c740260
P-BUS <pavlo.buslowski@gmail.com>
2023-04-15 21:07:51 +0300

    Body element created with Column, Image, Text

--- app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -16,20 +16,22 @@
 
 package com.codelab.basiclayouts
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -77,7 +79,19 @@ fun SearchBar(
 fun AlignYourBodyElement(
     modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+    Column(modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.ab1_inversions),
+            contentDescription = stringResource(id = R.string.ab1_inversions),
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(88.dp)
+        )
+        Text(
+            text = stringResource(R.string.ab1_inversions),
+            style = MaterialTheme.typography.h3
+        )
+    }
 }
 
 // Step: Favorite collection card - Material Surface
@@ -128,6 +142,7 @@ private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
 @Composable
 fun MySootheApp(modifier: Modifier) {
     SearchBar(modifier)
+    AlignYourBodyElement(modifier)
 }
 
 private val alignYourBodyData = listOf(
@@ -214,5 +229,6 @@ fun BottomNavigationPreview() {
 fun MySoothePreview() {
     MySootheTheme {
         MySootheApp(modifier = Modifier)
+        AlignYourBodyElement()
     }
 }
